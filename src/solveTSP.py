5f93c41a2f0361a71903b858b54de75806001f88
bitNathan <53419123+bitNathan@users.noreply.github.com>
2024-11-06 10:54:20 -0500

    added result saving

--- src/solveTSP.py
+++ src/solveTSP.py
@@ -19,7 +19,7 @@ def loadProblem(tsp_file, tour_file):
     return problem, solution
 
 
-def solveProblem(tsp_file, tour_file):
+def solveProblem(tsp_file, tour_file, save_file=None):
     print('Solving tsp for', tsp_file, '...')
     problem, solution = loadProblem(tsp_file, tour_file)
     start_time = time()
@@ -29,16 +29,35 @@ def solveProblem(tsp_file, tour_file):
     end_time = time()
     best_distance = problem.trace_tours(solution.tours)[0]
     time_taken = end_time - start_time
-
+    gap_percent = (calc_distance - best_distance)/calc_distance*100
+    
     print('Time taken (s):', time_taken)
     print('bayg29 Calculated Tour Length:', calc_distance)
     print('bayg29 actual Tour Length:', best_distance)
-    print('Gap %:', (calc_distance - best_distance)/calc_distance*100)
+    print('Gap %:', round(gap_percent, 2))
+    
+    if save_file:
+        open_mode = 'a' if os.path.exists(save_file) else 'w'
+        
+        with open(save_file, open_mode) as file:
+            file.write('{}\n'.format(tsp_file))
+            file.write('    Time taken (s): '+str(time_taken)+'\n')
+            file.write('    Calculated Tour Length:'+str(calc_distance)+'\n')
+            file.write('    Actual Tour Length: '+str(best_distance)+'\n')
+            file.write('    Gap %: '+str(round(gap_percent, 2))+'\n')
+        print('Results saved to', save_file)
+        
 
 
 if __name__ == '__main__':
     path = os.path.dirname(os.path.dirname(__file__))
+    timestamp = time()
+    
+    save_file = path+'/solutions/manual_solutions_'+str(timestamp)+'.txt'
+    
     solveProblem(path+'/data/test_data/bayg29.tsp',
-                 path+'/data/test_data/bayg29.opt.tour')
+                 path+'/data/test_data/bayg29.opt.tour',
+                 save_file)
     solveProblem(path+'/data/test_data/att48.tsp',
-                 path+'/data/test_data/att48.opt.tour')
+                 path+'/data/test_data/att48.opt.tour',
+                 save_file)
